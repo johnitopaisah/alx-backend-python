9eae88fb02c433a4a98c98f9d30b006b0f73d7e0
isahjohnitopa <isahjohna@gmail.com>
2024-10-16 14:14:35 +0200

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module that contains a single function that adds two integers.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Function taht add two integers together
+    Args:
+        a: First number to add
+        b: Second number to add
+    Returns:
+        The sum of the two numbers
+    """
+    return a + b
