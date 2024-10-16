40ef441a4fff51526f36375a043cd77bddff368b
isahjohnitopa <isahjohna@gmail.com>
2024-10-16 10:57:52 +0200

    Let's execute multiple coroutines at the same time with async

--- 0x01-python_async_function/1-main.py
+++ 0x01-python_async_function/1-main.py
@@ -3,3 +3,10 @@
 '''
 Test file for printing the correct output of the wait_n coroutine
 '''
+import asyncio
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+print(asyncio.run(wait_n(4, 5)))
+print(asyncio.run(wait_n(10, 7)))
+print(asyncio.run(10, 0)))
