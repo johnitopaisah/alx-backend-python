324bfe474f7f85e55b4ce16ae669ab84e64500fa
isahjohnitopa <isahjohna@gmail.com>
2024-10-16 14:42:41 +0200

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Contains a single function that calculates the
+floor of a number.
+"""
+import math
+
+
+def floor(n: floar) -> int:
+    """
+    Function to get the floor of a float
+    Args:
+        n: The floating number
+    Returns:
+        The floor of the floating number
+    """
+    return math.floor(n)
