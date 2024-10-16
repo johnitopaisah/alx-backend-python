2164ab7629bb2d4e0a29685fcee2c835ac2103a4
isahjohnitopa <isahjohna@gmail.com>
2024-10-16 15:00:40 +0200

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Contains a function that returns a string representation of a float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Module to convert int to string
+    Args:
+        n: The number to conver
+    Returns:
+        The string representation of the int
+    """
+    return str(n)
