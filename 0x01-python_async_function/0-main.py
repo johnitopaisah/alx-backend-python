178a3165a98961cd7e7d1a208086e68230c3bf5e
isahjohnitopa <isahjohna@gmail.com>
2024-10-16 10:39:25 +0200

    The basics of async

--- 0x01-python_async_function/0-main.py
+++ 0x01-python_async_function/0-main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
-
+'''
+Test file for printing the correct output of the wait_n coroutine
+'''
 import asyncio
 
 wait_random = __import__('0-basic_async_syntax').wait_random
---
+++ 0x01-python_async_function/1-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+'''
+Test file for printing the correct output of the wait_n coroutine
+'''
