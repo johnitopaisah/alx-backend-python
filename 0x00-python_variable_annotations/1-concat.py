7512dd754a561d27c324442c6c2beb49ad54dbef
isahjohnitopa <isahjohna@gmail.com>
2025-01-22 14:21:54 +0100

    first commint

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
-"""
-Module containing a single function that concatenates two
-strings.
-"""
+''' Description: takes a string str1 and a string str2 as arguments
+    and returns a concatenated string
+        parameters: str1: str
+               str2: str
+'''
 
 
 def concat(str1: str, str2: str) -> str:
-    """
-    Concatenates two strings.
-    Args:
-        str1: First string to concat
-        str2: Second string to concat
-    Returns:
-        The concatenated string
-    """
+    ''' Return concatenation of str1 and str2. '''
     return str1 + str2
